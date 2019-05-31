a401d33ca9c596c2febc344b2cac4917401df41a
Fibonacci1664 <dave.green1664@yahoo.co.uk>
2019-05-31 16:27:18 +0100

    First commit

---
+++ src/Switch.cpp
@@ -0,0 +1,36 @@
+/*
+ * Switch.cpp
+ *
+ *  Created on: 31 May 2019
+ *      Author: Dave
+ */
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+//	int value = 3;
+//
+//	switch (value)
+//	{
+//	case 1:
+//		cout << "The value is 1 " << endl;
+//		break;
+//	case 2:
+//		cout << "The value is 2 " << endl;
+//		break;
+//	case 3:
+//		cout << "The value is 3 " << endl;
+//		break;
+//	case 4:
+//		cout << "The value is 4 " << endl;
+//		break;
+//	default:
+//		cout << "Oops, something went wrong!" << endl;
+//	}
+
+	return 0;
+}
+
